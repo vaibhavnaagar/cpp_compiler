10baa86baf3b366bc3f188e0b16dc8d2c3dfbe33
Vaibhav Nagar <vaibhavn@iitk.ac.in>
2017-02-12 16:58:45 +0530

    test1 and test2 modified

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,3 +1,6 @@
+/*
+                        Functions and For loops
+*/
 #include <iostream>
 #include <cmath>
 
@@ -26,9 +29,6 @@ double ComputeSeriesValue(double x, int n) {
 }
 
 int main() {
-  cout << "This program is used to compute the value of the following series : " << endl;
-
-  cout << "(x^0)/0! + (x^1)/1! + (x^2)/2! + (x^3)/3! + (x^4)/4! + ........ + (x^n)/n! " << endl;
 
   cout << "Please enter the value of x : " ;
 
