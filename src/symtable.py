4042e12b90a94a0acb2ceecef5e3a1260c45224b
Vaibhav Nagar <vaibhavn@iitk.ac.in>
2017-03-16 01:19:04 +0530

    Symbol Table updated and new methods are added

--- src/symtable.py
+++ src/symtable.py
@@ -1,42 +1,47 @@
-global ScopeList
-ScopeList = [{"name":"Root", "parent" : "NULL",}]
- 
+global ScopeList, currentScope
+ScopeList = { "NULL" : None, "global" : {"name":"global", "parent" : "NULL",}, }
+currentScope = "global"
+
 class SymTab:
     def __init__(self):
         global ScopeList
         self.symtab = {}
        # self.numVar = 0
+
+       # I think, there is no need of this self.scope
         self.scope =  {
-                "name"       : ScopeList[-1]["name"],
-                "parent"     : ScopeList[-1]["parent"],
-                "tab"      : self,
+                "name"       : ScopeList[currentScope]["name"],
+                "parent"     : ScopeList[currentScope]["parent"],
+                "table"      : self,                          # No need, I guess
             }
-        ScopeList[-1]["tab"] = self
-        
-    def lookup(self,lexeme ):
+        ScopeList[currentScope]["table"] = self
+
+    def lookup(self, lexeme):
         if lexeme in self.symtab:
             return self.symtab[lexeme]
         return None
 
-    def lookupComplete(self,lexeme):
-        global ScopeList
-        for scope in reversed(ScopeList) :
-            a = scope["tab"].lookup(lexeme) 
-            if a != None:
-                return a
+    def lookupComplete(self, lexeme):
+        scope = ScopeList[currentScope]
+        while scope is not None:
+            entry = scope["table"].lookup(lexeme)
+            if entry != None:
+                return entry
+            scope = ScopeList[scope["parent"]]
         return None
 
-    def insertVar(self,name,type):
-        currtable = ScopeList[-1]["tab"]
-        print(currtable)
+    def insertID(self, name, id_type):
+        currtable = ScopeList[currentScope]["table"]
+        #print("[Symbol Table]", currtable.symtab)
         if currtable.lookup(name):
-            print("Entry already exists")
+            print("[Symbol Table] Entry already exists")
         else:
             currtable.symtab[name] = {
                 "name"      : name,
-                "type"      : type,
+                "type"      : id_type,
             }
-            #ScopeList[-1]["tab"].numVar += 1
+            print("[Symbol Table] Inserting new identifier: ", name, " type: ", id_type)
+            #ScopeList[-1]["table"].numVar += 1
 
     def addVarAttr(self,var,attribute,value):
         self.symtab[str(var)].update({attribute:value})
@@ -44,34 +49,60 @@ class SymTab:
     def addScopeAttr(self,attribute,value):
         self.scope.update({attribute:value})
 
-
-
     def addScope(self,name):
-        global ScopeList
-        scope = {
+        global ScopeList, currentScope
+        new_scope = {
                 "name"       : name,
-                "parent"     : ScopeList[-1]["name"],
+                "parent"     : ScopeList[currentScope]["name"],
+                "table"      : None
                 }
-        ScopeList.append(scope)
-        SymTab() 
+        ScopeList[name] =  new_scope
+        currentScope = name
+        SymTab()
+        print("[Symbol Table](addScope)", name)
 
-'''
-a = SymTab()
-a.addScope("main")
-print(ScopeList)
-a.insertVar("x","int")
-print(ScopeList[-1]["tab"].symtab)
-ScopeList[-1]["tab"].symtab["x"].update({"size":"4"})
-print(ScopeList[-1]["tab"].symtab)
-print(a.symtab)
-ScopeList[-1]["tab"].addVarAttr("x","value",10)
-print(ScopeList[-1]["tab"].symtab)
-ScopeList[-1]["tab"].addScopeAttr("type","function")
-print(ScopeList[-1]["tab"].scope)
-'''
+    def endScope(self):
+        """
+        Changes current scope to parent scope  when '}' is received
+        """
+        global currentScope
+        currentScope = ScopeList[currentScope]["parent"]
+        print("[Symbol Table](endScope)", currentScope)
+        if ScopeList[currentScope] is None:
+            print("[Symbol Table] Error: This line should not be printed")
+
+    #def deleteScope(self):
+    #    """
+    #    Deletes current scope (present at the last of list)
+    #    """
+    #    print("Removing Scope %s", ScopeList[-1]["name"])
+    #    del ScopeList[-1]
 
 
-    
+if __name__ == '__main__':
+    a = SymTab()
+    a.insertID("v", "CHAR")
+    a.addScope("main")
+    print(ScopeList)
+    a.insertID("x","INT")
+    print(ScopeList[currentScope]["table"].symtab)
+    ScopeList[currentScope]["table"].symtab["x"].update({"size":"4"})
+    a.insertID("x", "BOOL")
+    print(ScopeList[currentScope]["table"].symtab)
+    print(a.symtab)
+    ScopeList[currentScope]["table"].addVarAttr("x","value",10)
+    print(ScopeList[currentScope]["table"].symtab)
+    ScopeList[currentScope]["table"].addScopeAttr("type","function")
+    print(ScopeList[currentScope]["table"].scope)
+    a.endScope()
+    a.insertID("w", "FLOAT")
+    a.endScope()
+    if not a.lookupComplete("y"):
+        print("Not Exist")
+        pass
+
+
+
 
 '''
  a = SymTabEntry("x",5,"int","local")
@@ -84,7 +115,7 @@ print(ScopeList[-1]["tab"].scope)
 #       self.name = name
 #       self.parent = parent
 #       self.attribute = attribute
-#       self.tab = symtab
+#       self.table = symtab
 #  def getParent(self):
 #      return self.parent
 #
@@ -104,8 +135,8 @@ class SymTabEntry:
 class Scope:
     def __init__(self, name, parent, attribute,symtab):
         self. =
-     
-     
-     
+
+
+
 
          '''
